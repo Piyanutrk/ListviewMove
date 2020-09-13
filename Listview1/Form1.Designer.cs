97aa2bba800d8ee3fd1d9e53757724419f78abbf
piyanut.rk <piyanut.rk@gmail.com>
2020-09-13 18:07:49 +0700

    Add project files.

---
+++ Listview1/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Listview1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(22, 44);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(606, 289);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(553, 352);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(460, 352);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "NG";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 614);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Listview1/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Listview1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetupListview();
+        }
+
+        private void SetupListview()
+        {
+            listView1.View = View.Details;
+
+            listView1.AllowColumnReorder = false;
+
+            listView1.FullRowSelect = true;
+
+            listView1.GridLines = true;
+
+
+            listView1.Columns.Add("part", 80, HorizontalAlignment.Left);
+            listView1.Columns.Add("size", 80, HorizontalAlignment.Left);
+            listView1.Columns.Add("min/max", 200, HorizontalAlignment.Left);
+            listView1.Columns.Add("remain", 80, HorizontalAlignment.Left);
+            listView1.Columns.Add("ng/ok", 80, HorizontalAlignment.Center);
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    ListViewItem item1 = new ListViewItem("D");
+            //    if (i == 0)
+            //    {
+            //        item1.Selected = true;
+            //    }
+            //    item1.SubItems.Add("10");
+            //    item1.SubItems.Add("+0.002/+0.000");
+            //    item1.SubItems.Add("5");
+            //    listView1.Items.Add(item1);
+            //}
+
+            UpdateListview(AddListviewItem());
+        }
+
+        private List<ListItems> AddListviewItem()
+        {
+            List<ListItems> list = new List<ListItems>();
+            list.Add(new ListItems() { Part = "D", Size = "10", MinMax = "+0.005/+0.000", Remain = "5", Active = "active" });
+            list.Add(new ListItems() { Part = "L", Size = "10", MinMax = "+0.005/+0.000", Remain = "5" });
+            list.Add(new ListItems() { Part = "P", Size = "10", MinMax = "+0.005/+0.000", Remain = "5" });
+            list.Add(new ListItems() { Part = "W", Size = "10", MinMax = "+0.005/+0.000", Remain = "5" });
+            return list;
+        }
+
+        private void UpdateListview(List<ListItems> listItems)
+        {
+            listView1.Items.Clear();
+            foreach (var item in listItems)
+            {
+                ListViewItem item1 = new ListViewItem(item.Part);
+                if (item.Active != null)
+                {
+                    if (item.NgOk == "NG")
+                    {
+                        item1.BackColor = Color.Red;
+                    }
+                    else
+                    {
+                        item1.BackColor = Color.Blue;
+                        item1.ForeColor = Color.White;
+                    }
+                    //item1.Selected = true;
+                }
+
+                item1.SubItems.Add(item.Size);
+                item1.SubItems.Add(item.MinMax);
+                item1.SubItems.Add(item.Remain);
+                item1.SubItems.Add(item.NgOk);
+                listView1.Items.Add(item1);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var items = listView1.Items;
+
+            int index = 0;
+            for (int i = 0; i < items.Count; i++)
+            {
+                if (items[i].BackColor == Color.Blue)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            //int index = listView1.Items.IndexOf(listView1.SelectedItems[0]);
+            //int index = listView1.Items.IndexOf(listView1.SelectedItems[0]);
+
+            List<ListItems> list = new List<ListItems>();
+            //foreach (ListViewItem item in listView1.Items)
+            //{
+            //    list.Add(new ListItems() { Part = item[0].text, Size = item[0].SubItems[0].Text, MinMax = "+0.005/+0.000", Remain = "5" });
+            //}
+
+            for (int i = 0; i < items.Count; i++)
+            {                
+                list.Add(new ListItems()
+                {
+                    Part = items[i].Text,
+                    Size = items[i].SubItems[1].Text,
+                    MinMax = items[i].SubItems[2].Text,
+                    Remain = index == i ? (int.Parse(items[i].SubItems[3].Text) - 1).ToString() : items[i].SubItems[3].Text,
+                    NgOk = index == i ? "OK" : items[i].SubItems[4].Text == "OK" ? "OK" : "",
+                    Active = i > index && (index + 1 == i) ? "active" : index == items.Count - 1 && i == 0 ? "active" : null
+                });
+
+                //if (int.Parse(items[i].SubItems[3].Text) <= 0 && index == items.Count - 1)
+                //{
+                //    return;
+                //}
+            }
+
+            //list.Add(new ListItems() { Part = item[1].Text, Size = "10", MinMax = "+0.005/+0.000", Remain = "5" });
+            //list.Add(new ListItems() { Part = item[2].Text, Size = "10", MinMax = "+0.005/+0.000", Remain = "5" });
+            //list.Add(new ListItems() { Part = item[3].Text, Size = "10", MinMax = "+0.005/+0.000", Remain = "5" });
+
+            UpdateListview(list);
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+            {
+                return;
+            }
+
+            //int index = listView1.Items.IndexOf(listView1.SelectedItems[0]);
+            //if (listView1.Items[index].Focused == true)
+            //{
+            //    listView1.Items[index].BackColor = Color.Red;
+
+            //}
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var items = listView1.Items;
+
+            int index = 0;
+            for (int i = 0; i < items.Count; i++)
+            {
+                if (items[i].BackColor == Color.Green)
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            List<ListItems> list = new List<ListItems>();
+
+            //string text = "NG";
+
+            for (int i = 0; i < items.Count; i++)
+            {
+                list.Add(new ListItems()
+                {
+                    Part = items[i].Text,
+                    Size = items[i].SubItems[1].Text,
+                    MinMax = items[i].SubItems[2].Text,
+                    Remain = (int.Parse(items[i].SubItems[3].Text) - 1).ToString(),
+                    NgOk = items[i].SubItems[4].Text == "" && index == i ? "NG" : items[i].SubItems[4].Text == "NG" ? "NG" : "",
+                    //NgOk = index == items.Count - 1 ? "" : index == i ? "OK" : items[i].SubItems[4].Text != "" ? "OK" : "",
+                    Active = i == index ? "active" : null
+                    //Active = i > index && (index + 1 == i) ? "active" : index == items.Count - 1 && i == 0 ? "active" : null
+                });
+            }
+
+            UpdateListview(list);
+        }
+    }
+
+
+
+    public class ListItems
+    {
+        public string Part { get; set; }
+        public string Size { get; set; }
+        public string MinMax { get; set; }
+        public string Remain { get; set; }
+        public string NgOk { get; set; }
+        public string Active { get; set; }
+    }
+}
